5fce2e6119e9a70dd11cde1b3c7a2d4dbb20578a
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-01-09 18:02:28 +0800

    fix: 修复报错

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,6 @@ import { persist, store } from "./store";
 function App({ children }: PropsWithChildren<any>) {
   <Provider store={store}>
     <PersistGate loading={null} persistor={persist}>
-      {/* 解决taro小程序 onLoad加载时候获取不到页面实例的情况 */}
-      {/*eslint-disable-next-line*/}
       {(_isLoading) => useCustomRouter()}
     </PersistGate>
   </Provider>;
