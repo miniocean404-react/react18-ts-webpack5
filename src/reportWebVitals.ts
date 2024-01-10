f4ae3d40ad7f027b976f1a22303d6925cd58a529
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-01-10 16:34:38 +0800

    doc: 添加注释

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,3 +1,5 @@
+// 测量性能 使用 example: reportWebVitals(console.log)
+// 发送报告 查看更多: https://bit.ly/CRA-vitals
 const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(({ onCLS, onFID, onFCP, onLCP, onTTFB }) => {
