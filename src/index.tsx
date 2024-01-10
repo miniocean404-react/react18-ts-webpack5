684760acca65ba4a2c0565a585a89cc7a021a974
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-01-10 16:50:11 +0800

    chore: 修改 root

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import { createRoot } from "react-dom/client"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom"
 
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { persist, store } from "./store";
+import { Provider } from "react-redux"
+import { PersistGate } from "redux-persist/integration/react"
+import { persist, store } from "./store"
 
-import "./css/base/base.scss";
+import "./css/base/base.scss"
 
-const root = document.getElementById("root")!;
+const root = document.getElementById("root")!
 
 createRoot(root).render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persist}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persist}>
+      <BrowserRouter>
         <App />
-      </PersistGate>
-    </Provider>
-  </BrowserRouter>,
-);
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+)
 
-reportWebVitals(null);
+reportWebVitals(null)
